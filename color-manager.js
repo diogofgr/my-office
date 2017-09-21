8c27a1b102f3b6580ad3ba08ca0a6ed9db03699f
Diogo Farias Rodrigues <diogofgr@gmail.com>
2017-09-21 17:10:39 +0100

    add cube with btn to change color

---
+++ color-manager.js
@@ -0,0 +1,37 @@
+AFRAME.registerComponent('color-manager', {
+  init: function () {
+
+  },
+  changeColor: function () {
+    var el = this.el;
+    var currentColor = el.getAttribute('color');
+    console.log('change ball color');
+    el.setAttribute('color', nextColor(currentColor));
+
+    function nextColor(currentColor) {
+    const colors = ['aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'azure','beige', 'bisque', 'black', 'blanchedalmond', 'blue', 'blueviolet','brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral','cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan','darkgoldenrod', 'darkgray', 'darkgreen', 'darkgrey', 'darkkhaki', 'darkmagenta','darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen','darkslateblue', 'darkslategray', 'darkslategrey', 'darkturquoise', 'darkviolet','deeppink', 'deepskyblue', 'dimgray', 'dimgrey', 'dodgerblue', 'firebrick','floralwhite', 'forestgreen', 'fuchsia', 'gainsboro', 'ghostwhite', 'gold','goldenrod', 'gray', 'green', 'greenyellow', 'grey', 'honeydew', 'hotpink','indianred', 'indigo', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen','lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow', 'lightgray','lightgreen', 'lightgrey', 'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue','lightslategray', 'lightslategrey', 'lightsteelblue', 'lightyellow', 'lime', 'limegreen','linen', 'magenta', 'maroon', 'mediumaquamarine', 'mediumblue', 'mediumorchid','mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise','mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin', 'navajowhite','navy', 'oldlace', 'olive', 'olivedrab', 'orange', 'orangered', 'orchid','palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff','peru', 'pink', 'plum', 'powderblue', 'purple', 'rebeccapurple', 'red', 'rosybrown','royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell','sienna', 'silver', 'skyblue', 'slateblue', 'slategray', 'slategrey', 'snow','springgreen', 'steelblue', 'tan', 'teal', 'thistle', 'tomato', 'turquoise','violet', 'wheat', 'white', 'whitesmoke', 'yellow', 'yellowgreen'];
+
+      var currentIndex = colors.indexOf(currentColor);
+      return colors[currentIndex + 1];
+    }
+  }
+});
+
+AFRAME.registerComponent('btn-change-color', {
+  schema: {
+    color: {default: 'green'}
+  },
+
+  init: function () {
+    var data = this.data;
+    // console.log(data.color);
+    var el = this.el;
+    const ColorManager = document.querySelector('[color-manager]').components['color-manager'];
+
+    el.addEventListener('click', function () {
+      el.parentElement.emit('squeezeanimation');
+      ColorManager.changeColor();
+    });
+    // console.log('start color manager.');
+  },
+});
